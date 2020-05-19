ee02fa0609ed1b34668e724433992caf81612c3d
17189 <17189@DESKTOP-LNCHGF7>
2020-05-18 22:11:32 -0400

    Initial test

---
+++ ming-sophia/ming-sophia/src/main/java/com/mycompany/ming/sophia/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ming.sophia;
+
+/**
+ *
+ * @author 17189
+ */
+public class Main {
+    public static void main(String [] Args){
+        System.out.print("hello world");
+    }
+    
+}
